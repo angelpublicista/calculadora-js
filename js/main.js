55dc5d8e9fade6b4ab3a598e54d0f11a107f6d96
Angel Burgos <angellpublicista.1@gmail.com>
2019-12-05 07:00:44 -0500

    Avance 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-const keys = document.querySelectorAll('.key')
+const keys = document.getElementById('calculator-keys')
 const screen = document.getElementById('screen')
 
-for (const key of keys) {
-    key.addEventListener('click',function(){
-        let content = this.innerHTML;
-        
-        screen.innerHTML = content
+function printScreen(number){
+    let num = number.textContent
+    if (screen.textContent == '0') {
+        screen.textContent = num
+    } else {
+        screen.textContent += num
+    } 
+}
+
+function clearScreen(){
+    screen.textContent = 0
+}
+
+function addOper(element, number){
+    number1 = number
+    typeOper = 'addOper'
+    screen.textContent = element.textContent
+    return {number1, typeOper}
+}
+
+const calculatorFunc = () => {
+    keys.addEventListener('click', function(e){
+        let element = e.target
+        let oper = element.getAttribute('data-oper')
+
+        if(oper == 'number'){
+            printScreen(element)
+        }
+
+        if (oper == 'draft') {
+            clearScreen()
+        }
+
+        if (oper == 'sum') {
+            addOper(element, screen.textContent)
+        }
     })
 }
+
+calculatorFunc()
